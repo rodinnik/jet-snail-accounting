b3712e560fff9ab42804facef5885cddb0164a83
rodinnik <lkjhgbvlm@mail.ru>
2022-12-17 21:25:23 +0400

    edit title

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Hello Blade</title>
+    <title>jet snail accounting</title>
     @vite('resources/sass/app.scss')
 </head>
 
